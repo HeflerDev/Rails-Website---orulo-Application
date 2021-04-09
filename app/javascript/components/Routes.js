f0d60a1eff3c48e61f44ff26c7956da5b7b2876f
heflerdev <heflerdev@gmail.com>
2021-04-09 19:05:41 -0300

    Update Routing

--- app/javascript/components/Routes.js
+++ app/javascript/components/Routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './App';
 import Navbar from './Navbar';
+import Showcase from '../containers/Showcase';
 
 const Routes = () => (
   <BrowserRouter>
     <Navbar />
     <Switch>
       <Route exact path="/" component={App} />
+      <Route path="/query/:page" component={Showcase} />
     </Switch>
   </BrowserRouter>
 );
--- app/javascript/containers/Showcase.js
+++ app/javascript/containers/Showcase.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Case from '../components/Case';
+import fetchData from '../logic/fetchData';
 
-const Showcase = () => {
+const Showcase = ({ match: { params: { page } } }) => {
   const [stateData, setStateData] = useState({
     data: null,
     isLoading: true,
@@ -10,12 +13,7 @@ const Showcase = () => {
 
   useEffect(() => {
     setStateData({...stateData, isLoading: true});
-    fetch('https://www.orulo.com.br/api/v2/buildings', {
-      method: 'GET',
-      headers: {
-        Authorization: 'Bearer VDqfisFJS9-8yiLluw3fvII-lILi7WjHNjDGAg9jMbU'
-      },
-   })
+    fetchData.fromBuildings(page)
     .then(res => {
       if (res.ok) {
         return res.json();
@@ -28,7 +26,35 @@ const Showcase = () => {
       .catch((err) => {
         setStateData({...stateData, error: err, isLoading: false});
       });
-  }, []);
+  }, [page]);
+
+  const renderNavButtons = (current, total) => {
+    if (current > 1) {
+      if (current >= total) {
+        return (
+          <>
+            <Link to={`/query/${current - 1}`} >{ current - 1 }</Link>
+            <Link to={`/query/${current}`} >{ current }</Link>
+          </>
+        );
+      }
+      return (
+        <>
+          <Link to={`/query/${current - 1}`} >{ current - 1 }</Link>
+          <Link to={`/query/${current}`} >{ current }</Link>
+          <Link to={`/query/${current + 1}`}>{ current + 1}</Link>
+          <Link to={`/query/${total}`}>{ `...${total}` }</Link>
+        </>
+      )
+    }
+    return (
+      <>
+        <Link to={`/query/${current}`} >{ current }</Link>
+        <Link to={`/query/${current + 1}`}>{ current + 1}</Link>
+        <Link to={`/query/${total}`}>{ `...${total}` }</Link>
+      </>
+    );
+  }
 
   if (stateData.isLoading) {
     return <p>Loading...</p>
@@ -49,8 +75,27 @@ const Showcase = () => {
           ))
         }
       </div>
+      <div className="queue center bottom-nav-container">
+        { renderNavButtons(stateData.data.page, stateData.data.total_pages) }
+      </div>
     </div>
   );
+}
+
+Showcase.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      page: PropTypes.string,
+    }),
+  }),
+}
+
+Showcase.defaultProps = {
+  match: {
+      params: {
+        page: '1',
+      },
+    },
 }
 
 export default Showcase;
