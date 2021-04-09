0600aeb5d5dc382c887b7e165b489b0779c6e232
heflerdev <heflerdev@gmail.com>
2021-04-09 19:04:59 -0300

    Create fetch helper

---
+++ app/javascript/logic/fetchData.js
@@ -0,0 +1,38 @@
+const authKey = 'Bearer VDqfisFJS9-8yiLluw3fvII-lILi7WjHNjDGAg9jMbU';
+
+const fetchData = {
+  fromBuildings(page) {   
+    return (
+      fetch(`https://www.orulo.com.br/api/v2/buildings?results_per_page=12&page=${page}`, {
+        method: 'GET',
+        headers: {
+          Authorization: authKey 
+        },
+       })
+    );
+  },
+
+  fromAdressesCity(state) {
+    return (
+      fetch(`https://www.orulo.com.br/api/v2/addresses/cities?state=${state}`, {
+        method: 'GET',
+        headers: {
+          Authorization: authKey
+        },
+      })
+    );
+  },
+
+  fromAdressesArea(state, city) {
+    return (
+      fetch(`https://www.orulo.com.br/api/v2/addresses/areas?state=${state}&city=${city}`, {
+        method: 'GET',
+        headers: {
+          Authorization: authKey
+        },
+      })
+    );
+  }
+}
+
+export default fetchData;
