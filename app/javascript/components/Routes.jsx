bd409a76c21569d9a3f87d2692fe08fca3d9177e
heflerdev <heflerdev@gmail.com>
2021-04-11 01:34:25 -0300

    Update: Routing/POST

--- app/javascript/components/Routes.jsx
+++ app/javascript/components/Routes.jsx
@@ -4,12 +4,14 @@ import App from './App';
 import Navbar from './Navbar';
 import Showcase from '../containers/Showcase';
 import StateProfile from '../containers/StateProfile';
+import NewUserForm from '../containers/NewUserForm';
 
 const Routes = () => (
   <BrowserRouter>
     <Navbar />
     <Switch>
       <Route exact path="/" component={App} />
+      <Route exact path="/user" component={NewUserForm} />
       <Route path="/profiles/:id" component={StateProfile} />
       <Route path="/query/:page" component={Showcase} />
     </Switch>
